9f9a66032555fb4821109f85fe21d4e9e309a805
edelbarno <barnoedel12@gmail.com>
2024-03-21 15:57:34 +0300

    add arithmetic lab

--- index.js
+++ index.js
@@ -1,2 +1,13 @@
+let num1=31;
+let num2=2;
+var multiply= num1 * num2;
 
-// Write your code here
+let random ;
+random= Math.floor(Math.random() * 10) + 1;   
+
+let mod;
+let num3= 22;
+let num4= 9;
+mod= num3%num4;
+
+const max= Math.max(1,4,20);
